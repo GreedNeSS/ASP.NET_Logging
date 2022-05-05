b06fadbcfe61a03e3677f325e9029b06c06f1a75
GreedNeSS <ruslan199126@gmail.com>
2022-05-05 20:29:02 +0300

    Added ILogger example.

--- ILoggerMiddleware/Program.cs
+++ ILoggerMiddleware/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Map("/", (ILogger<Program> logger, HttpContext context) =>
+{
+    logger.LogCritical($"Critical message {context.Request.Path}");
+    logger.LogDebug($"Debug message {context.Request.Path}");
+    logger.LogError($"Error message {context.Request.Path}");
+    logger.LogInformation($"Information message {context.Request.Path}");
+    logger.LogTrace($"Trace message {context.Request.Path}");
+    logger.LogWarning($"Warning message {context.Request.Path}");
+    return "Hello User";
+});
 
 app.Run();
