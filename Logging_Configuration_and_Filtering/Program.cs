95b64181a8a5e5faa44f9884b1462649980a3cf1
GreedNeSS <ruslan199126@gmail.com>
2022-05-06 22:36:10 +0300

    Added filter example.

--- Logging_Configuration_and_Filtering/Program.cs
+++ Logging_Configuration_and_Filtering/Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.Extensions.Logging.Console;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+
+builder.Services.AddLogging(builder =>
+{
+    builder.AddConsole();
+    builder.AddFilter<ConsoleLoggerProvider>("Program", LogLevel.Trace);
+});
 var app = builder.Build();
 
-app.Map("/", (ILogger<Program> logger, HttpContext context) =>
+
+app.Map("/", (ILogger<Program> logger) =>
 {
-    logger.LogCritical($"Critical message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
-    logger.LogDebug($"Debug message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
-    logger.LogError($"Error message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
-    logger.LogInformation($"Information message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
-    logger.LogTrace($"Trace message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
-    logger.LogWarning($"Warning message {context.Request.Path}, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogCritical($"Critical message, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogDebug($"Debug message, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogError($"Error message, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogInformation($"Information message, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogTrace($"Trace message, Time: {DateTime.Now.ToLongTimeString()}");
+    logger.LogWarning($"Warning message, Time: {DateTime.Now.ToLongTimeString()}");
     return "Hello User";
 });
 
